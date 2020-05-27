c758a9d6aa6a268defae3f80ab4fc604a0056d52
purpisluv <andreylov722@gmail.com>
2020-05-28 04:16:26 +0700

    (верстка) верстка новостей

---
+++ src/components/News/MainNews.vue
@@ -0,0 +1,150 @@
+<template lang="pug">
+  .News
+    .News-head
+      .container
+        .row
+          .title Новости:
+          .arrows
+            .button.left
+              img(src='@/assets/image/News/Arrow-left.svg')
+            .button.right
+              img(src='@/assets/image/News/Arrow-right.svg')
+    .News-body
+      .container
+        .cards
+          person-card(:personCardData="personCards[0]")
+          info-card(:infoCardData="infoCards[0]")
+          person-card(:personCardData="personCards[1]")
+          info-card(:infoCardData="infoCards[1]")
+</template>
+
+<script>
+import PersonCard from "@/components/News/PersonCard";
+import InfoCard from "@/components/News/InfoCard";
+
+export default {
+  components: {
+    PersonCard,
+    InfoCard
+  },
+  name: "MainNews",
+  data() {
+    return {
+      infoCards: [
+        {
+          imgName: "card-1.png",
+          subtitle:
+            "Краудфандинг является одной из наиболее активно развивающихся финансовых систем."
+        },
+        {
+          imgName: "card-2.png",
+          subtitle:
+            "Сегодня в этой сфере организовано свыше 30 платформ коллективного финансирования."
+        }
+      ],
+      personCards: [
+        {
+          title: "Уварова Татьяна",
+          subtitle:
+            "Сейчас краудфандинг является одной из наиболее активно развивающихся финансовых систем в России. За прошедшие годы работы с помощью платформ российского краудфандинга был собран 1 миллиард рублей.",
+          date: "12.05.2020",
+          isSubscribe: true
+        },
+        {
+          title: "Семенов Константин",
+          subtitle:
+            "Сегодня в этой сфере организовано свыше 30 платформ коллективного финансирования, многие из которых ориентированы на развитие определенных направлений — творческих, технологических, социальных и т.д.",
+          date: "12.05.2020",
+          isSubscribe: false
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/settings.scss";
+
+@mixin main($maxWidth) {
+  .News {
+    display: flex;
+    flex-direction: column;
+  }
+  .row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 1rem 0;
+    background-color: $White;
+  }
+  .title {
+    @include afs(32px, 28px, 24px);
+    font-family: Gotham Pro;
+    font-style: normal;
+    font-weight: bold;
+    line-height: 31px;
+    color: $Main;
+  }
+  .arrows {
+    display: flex;
+  }
+  .button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 101px;
+    height: 34px;
+    cursor: pointer;
+  }
+  .left {
+    background: linear-gradient(
+      180deg,
+      $White 0%,
+      rgba(244, 244, 244, 0.47) 100%
+    );
+  }
+  .right {
+    background: linear-gradient(
+      180deg,
+      $WhiteSecond 0%,
+      rgba(244, 244, 244, 0.47) 100%
+    );
+  }
+  .News-body {
+    background: linear-gradient(
+      351.01deg,
+      $SilverSecond -88.68%,
+      #fbfbfc 117.44%
+    );
+  }
+  .cards {
+    display: flex;
+    @if ($maxWidth <= $TabletWidth) {
+      justify-content: space-around;
+    } @else {
+      justify-content: space-between;
+    }
+    flex-wrap: wrap;
+  }
+}
+
+@media screen and (min-width: 0px) and (max-width: $PhoneWidth) {
+  @include main(0px);
+}
+@media screen and (min-width: 0px) and (max-width: $PhoneWidth) {
+  @include main($PhoneWidth);
+}
+@media screen and (min-width: $PhoneWidth) and (max-width: $TabletWidth) {
+  @include main($TabletWidth);
+}
+@media screen and (min-width: $TabletWidth) and (max-width: $SmDesktopWidth) {
+  @include main($SmDesktopWidth);
+}
+@media screen and (min-width: $SmDesktopWidth) and (max-width: $DesktopWidth) {
+  @include main($DesktopWidth);
+}
+@media screen and (min-width: $DesktopWidth) {
+  @include main($DesktopWidth);
+}
+</style>
---
+++ src/components/News/PersonCard.vue
@@ -0,0 +1,124 @@
+<template lang="pug">
+  .PersonCard
+    .row
+      .btn(ref='cardButton')
+        img(src='@/assets/image/News/Plus.svg')
+    .row
+      .title {{ personCardData.title }}
+    .row
+      .subtitle {{ personCardData.subtitle }}
+    .row
+      .date {{ personCardData.date }}
+      a.connect(href='#')
+        img(src='@/assets/image/News/Link.svg')
+</template>
+
+<script>
+export default {
+  name: "PersonCard",
+  props: {
+    personCardData: Object
+  },
+  methods: {
+    stylizeSubscribeButton: function() {
+      this.personCardData.isSubscribe
+        ? this.$refs.cardButton.classList.add("subscribe")
+        : this.$refs.cardButton.classList.add("unsubscribe");
+    }
+  },
+  mounted() {
+    this.stylizeSubscribeButton();
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/settings.scss";
+
+@mixin main($maxWidth) {
+  .PersonCard {
+    margin: 3rem 0;
+    width: 270px;
+    height: 360px;
+    background-color: $White;
+    border-radius: 3px;
+    box-shadow: 2px 4px 10px rgba(0, 0, 0, 0.1);
+    font-family: Futura PT;
+    font-style: normal;
+  }
+  .row {
+    display: flex;
+    align-items: center;
+  }
+  .btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: auto;
+    width: 34px;
+    height: 34px;
+    border-radius: 3px;
+    cursor: pointer;
+  }
+  .subscribe {
+    background-color: $SilverFourth;
+  }
+  .unsubscribe {
+    background: linear-gradient(
+      59.09deg,
+      $BlueSecond 18.91%,
+      $AquaFirst 150.1%
+    );
+  }
+  .title {
+    margin: 1.5rem auto;
+    @include afs(18px, 16px, 14px);
+    font-weight: 450;
+    line-height: 23px;
+    color: $Main;
+  }
+  .subtitle {
+    margin: 0 auto;
+    @include afs(14px, 12px, 10px);
+    width: 174px;
+    height: 200px;
+    font-weight: normal;
+    line-height: 18px;
+    color: $SilverThird;
+    text-align: center;
+    overflow: hidden;
+  }
+  .date {
+    @include afs(12px, 10px, 8px);
+    font-weight: 450;
+    line-height: 15px;
+    margin: 1rem;
+  }
+  .connect {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 1rem;
+    margin-left: auto;
+  }
+}
+
+@media screen and (min-width: 0px) and (max-width: $PhoneWidth) {
+  @include main(0px);
+}
+@media screen and (min-width: 0px) and (max-width: $PhoneWidth) {
+  @include main($PhoneWidth);
+}
+@media screen and (min-width: $PhoneWidth) and (max-width: $TabletWidth) {
+  @include main($TabletWidth);
+}
+@media screen and (min-width: $TabletWidth) and (max-width: $SmDesktopWidth) {
+  @include main($SmDesktopWidth);
+}
+@media screen and (min-width: $SmDesktopWidth) and (max-width: $DesktopWidth) {
+  @include main($DesktopWidth);
+}
+@media screen and (min-width: $DesktopWidth) {
+  @include main($DesktopWidth);
+}
+</style>
